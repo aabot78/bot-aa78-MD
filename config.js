293ef3988c7d6397652515cd98e2764d4a204804
aabot78 <89217561+aabot78@users.noreply.github.com>
2022-05-25 18:11:51 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,51 @@
+
+//═══════[modules]════════\\
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//═══════[api website]════════\\
+global.APIs = {
+	zenz: 'https://zenzapi.xyz', //credit zenz, thank you so much bro <3
+}
+//═══════[api key of the website]════════\\
+global.APIKeys = {
+	'https://zenzapi.xyz': '805a6c3fa9', //thank you to zenz
+}
+
+//═══════[modification]════════\\
+global.owner = ['33758288938','46764534862'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.mynum = ['2126535462056@s.whatsapp.net'] //another owner number
+global.premium = ['46764534862'] //premium number
+global.pengguna = 'AA' //username
+global.botnma = 'AA' //bot name
+global.ownernma = 'AA' //owner name
+global.packname = '😎' //sticker package name
+global.author = 'AA' //sticker author name
+global.sessionName = 'session' //session name
+global.prefa = ['!'] //prefix
+global.sp = '⭔' //design
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature in only for the bot number',
+    wait: 'In process...',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+global.limitawal = {
+    premium: "Infinity", //premium user limit
+    free: 100 //free user limit
+}
+global.thumb = fs.readFileSync('./XeonMedia/cheemspic.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
