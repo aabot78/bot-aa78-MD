3c94b1148fd8179caeb956921c714b6b29fd9ce4
aabot78 <89217561+aabot78@users.noreply.github.com>
2022-06-18 19:49:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['994406094410','33758387451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.owner = ['994406094410','33758387451','962777844696] //owner number, u can put multiple owner number, within quotations and seperated by comma.
 global.mynum = ['33758387451@s.whatsapp.net'] //another owner number
 global.premium = ['33758387451'] //premium number
 global.pengguna = 'AA' //username
