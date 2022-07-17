c8072a9ec4baa1929a02be260a27531175a55d5a
aabot78 <89217561+aabot78@users.noreply.github.com>
2022-07-18 01:58:52 +0200

    Update XeonCheems.js

--- XeonCheems.js
+++ XeonCheems.js
@@ -230,17 +230,8 @@ ${groupName}`})
 		await XeonBotInc.groupParticipantsUpdate(i, [users], 'remove')
 		await XeonBotInc.updateBlockStatus(users, 'block')
 		  }
-  	}
-if (!fakeadmin) return
-		let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
-		let allchats = await store.chats.all().filter(v => v.id.endsWith('@g.us')).map(v => v.id)
-		for (let i of allchats) {
-		  if (users === botNumber) return 
-		await XeonBotInc.groupParticipantsUpdate(i, [users], 'remove')
-		await XeonBotInc.updateBlockStatus(users, 'block')
-	    }
-        }
-  
+  	} 
+       }
 	break
     case prefix+'add': case prefix+'a': {
 		if (!m.isGroup) return
