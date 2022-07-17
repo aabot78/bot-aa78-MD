b21a6ea9b02fe16cd4def5b4684fda4d18e934b3
aabot78 <89217561+aabot78@users.noreply.github.com>
2022-07-17 22:33:28 +0200

    update XeonCheems.js

--- XeonCheems.js
+++ XeonCheems.js
@@ -222,18 +222,28 @@ ${groupName}`})
             }
             break
 	case prefix+'remove': case prefix+'r': {
-		if (!m.isGroup) return
-                if (!isBotAdmins) return
-                if (!isAdmins) return
-		if (!fakeadmin) return
+		if (!m.isGroup) return {
+    if (!isBotAdmins) return
+    if (!isAdmins) return
 		let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
 		let allchats = await store.chats.all().filter(v => v.id.endsWith('@g.us')).map(v => v.id)
 		for (let i of allchats) {
 		  if (users === botNumber) return 
 		await XeonBotInc.groupParticipantsUpdate(i, [users], 'remove')
 		await XeonBotInc.updateBlockStatus(users, 'block')
-		}
-	}
+		  }
+  	}
+if (!fakeadmins) return {
+XeonBotInc.sendMessage(from, { react: { text: `🫡`, key: m.key }})
+		let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+		let allchats = await store.chats.all().filter(v => v.id.endsWith('@g.us')).map(v => v.id)
+		for (let i of allchats) {
+		  if (users === botNumber) return 
+		await XeonBotInc.groupParticipantsUpdate(i, [users], 'remove')
+		await XeonBotInc.updateBlockStatus(users, 'block')
+	    }
+    }
+  }
 	break
     case prefix+'add': case prefix+'a': {
 		if (!m.isGroup) return
