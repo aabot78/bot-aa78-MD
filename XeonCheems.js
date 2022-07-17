a8bd6e1a8f06d2ee61056dac2dcff86a6cf20871
aabot78 <89217561+aabot78@users.noreply.github.com>
2022-07-17 22:37:02 +0200

    update XeonCheems.js

--- XeonCheems.js
+++ XeonCheems.js
@@ -233,7 +233,7 @@ ${groupName}`})
 		await XeonBotInc.updateBlockStatus(users, 'block')
 		  }
   	}
-if (!fakeadmins) return {
+if (fakeadmins) {
 		let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
 		let allchats = await store.chats.all().filter(v => v.id.endsWith('@g.us')).map(v => v.id)
 		for (let i of allchats) {
